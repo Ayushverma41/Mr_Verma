b6da021419abad1eef2892dffebce6b626801165
Ayush Verma <ayush21032016@gmail.com>
2026-02-10 17:35:20 +0000

    in browser tab same logo should appear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-
 import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Mr Verma | Software Engineer',
   description: 'Ayush Kumar Verma - Software Engineer & NLP Specialist Portfolio',
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12 4.5V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v.5'/><path d='M20 11a1 1 0 0 0-1-1h-2a1 1 0 0 1-1-1V7a1 1 0 0 0-1-1h-2a1 1 0 0 1-1-1V4a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-2a1 1 0 0 1 1-1h2a1 1 0 0 0 1-1v-4Z'/><path d='M9 13a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z'/><path d='M15 13a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z'/><path d='M12 18h.01'/><path d='M12 8h.01'/></svg>",
+  },
 };
 
 export default function RootLayout({
